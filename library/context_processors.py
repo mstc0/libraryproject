454059e877ee64edadc3255272a5ed34cd180ab1
eltaco0 <137419488+eltaco0@users.noreply.github.com>
2023-07-18 16:02:58 +0200

    Fixed context processor

--- library/context_processors.py
+++ library/context_processors.py
@@ -1,13 +1,12 @@
 from library import models
+from django.core.exceptions import ObjectDoesNotExist
 from .cart import Cart
 
 def get_profile(req):
-    cart = Cart(req).cart.keys()
-    try:
-        profile_extra = models.UserExtraProfile.objects.get(user_id=req.user.id)
-        # owned_games = profile_extra.owned_games.all()
-        # wish_list = profile_extra.wish_list.all()
-    except:
-        profile_extra = ""
-
-    return {'user_extra': profile_extra, 'cart': cart}
+    if req.user.is_authenticated:
+        profile_extra, created = models.UserExtraProfile.objects.get_or_create(user_id=req.user.id)
+        if created:
+            profile_extra.display_name = req.user.username
+            profile_extra.save()
+        return {'user_extra': profile_extra, 'cart': Cart(req).cart.keys()}
+    return {}
